e6eb245c96eeda0738cf9fb3ead16689d59efdc7
jacerowe <jacerowe2012@gmail.com>
2026-02-26 13:32:30 -0500

    Cache bust v5, sword cooldown 0.001s

--- src/entities/Sword.js
+++ src/entities/Sword.js
@@ -6,7 +6,7 @@ class Sword {
     this.restState = 'UP';
     this.swinging = false;
     this.cooldown = 0;
-    this.cooldownDuration = 0.03;
+    this.cooldownDuration = 0.001;
     this.swingTimer = 0;
     this.swingDuration = 0.18;
     this.activeFrameStart = 0.04;
